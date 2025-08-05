4e78c7fa4daec9cfffa36a204f25059a38fa2833
shiwani08 <shiwanisoni29082002@gmail.com>
2025-08-05 18:18:12 +0530

    Learning about inner working

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,10 @@
 import { log } from './logger.js';
+import * as os from 'os';
 // console.log(module);
-console.log(log);
+// console.log(log);
+
+const total = os.totalMemory();
+const free = os.freemem();
+
+console.log(`Total Memory: ${total}`);
+console.log(`Free Memory: ${free}`);
--- backend/logger.js
+++ backend/logger.js
@@ -1,6 +1,12 @@
-var url = 'https://example.com/api/notes';
-function log(message) {
+(function (exports, require, module, __filename, __dirname) {
+    console.log(__dirname);
+    console.log(__filename);
+  var url = "https://example.com/api/notes";
+  function log(message) {
     console.log(message);
-}
-const logging = log;
-export { logging as log };
+  }
+  const logging = log;
+  // Removed export statement from inside IIFE
+})();
+
+export function log(message) { return console.log(message); }
