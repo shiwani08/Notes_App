ca5bbe4cbd266dbf40d22538c5ad0091127e7a30
shiwani08 <shiwanisoni29082002@gmail.com>
2025-08-06 13:09:10 +0530

    Events

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,38 @@
-import { log } from './logger.js';
-import * as os from 'os';
+//synchronous means block the thread until the operation is complete
+//asynchronous means non-blocking, it will not block the thread
+
+import { log } from "./logger.js";
+import * as os from "os";
+import fs from "fs";
+import { EventEmitter } from "events";
+
 // console.log(module);
 // console.log(log);
+const eventEmitter = new EventEmitter();
 
-const total = os.totalMemory();
+// this is the callback function which is listening to the event
+eventEmitter.on("This is the message", (arg) => {
+  console.log("Listener called", arg);
+});
+
+const total = os.totalmem();
 const free = os.freemem();
 
-console.log(`Total Memory: ${total}`);
-console.log(`Free Memory: ${free}`);
+// console.log(`Total Memory: ${total}`);
+// console.log(`Free Memory: ${free}`);
+
+// sync programming
+console.log(`File details from Sync: ${fs.readdirSync(".")}`);
+
+// async programming
+fs.readdir(".", (err, files) => {
+  if (err) {
+    console.error("Error reading directory:", err);
+    return;
+  }
+  console.log(`File details from Async: ${files}`);
+});
+
+// hello world is the event trigger
+eventEmitter.emit("This is the message", "Hello world!");
+console.log(`Event Emitter: ${eventEmitter.eventNames()}`);
